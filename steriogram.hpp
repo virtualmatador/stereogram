28bcefc69eeee0aa6d857527ecde7c25988c35c2
Ali Asadpoor <virtualmatador@yahoo.com>
2019-07-28 18:53:34 -0500

    Support custom order of rgba

--- steriogram.hpp
+++ steriogram.hpp
@@ -12,43 +12,51 @@ namespace steriogram
     }
 
     template<int PIXEL_SIZE>
-    std::vector<unsigned char> CreateCurvedPattern(const int column)
+    std::vector<unsigned char> CreateCurvedPattern(const int column, const int order_rgba)
     {
-        std::vector<unsigned char> pattern(column * column * PIXEL_SIZE);
+        int r = (order_rgba & 0xFF000000) >> 24;
+        int g = (order_rgba & 0x00FF0000) >> 16;
+        int b = (order_rgba & 0x0000FF00) >>  8;
+        int a = (order_rgba & 0x000000FF) >>  0;
+        std::vector<unsigned char> pattern(column * column * 4);
         for (int y = 0; y < column; ++y)
         {
             for (int x = 0; x < column; ++x)
             {
-                if (PIXEL_SIZE > 0)
-                    pattern[(y * column + x) * PIXEL_SIZE + 0] = int(pow(y + column / 3.6, 1.45) * pow(x + column / 3.4, 1.55)) % 256;
-                if (PIXEL_SIZE > 1)
-                    pattern[(y * column + x) * PIXEL_SIZE + 1] = int(pow(y + column / 4.8, 1.15) * pow(x + column / 4.6, 1.25)) % 256;
-                if (PIXEL_SIZE > 2)
-                    pattern[(y * column + x) * PIXEL_SIZE + 2] = int(pow(y + column / 2.2, 1.85) * pow(x + column / 2.4, 1.75)) % 256;
-                if (PIXEL_SIZE > 3)
-                    pattern[(y * column + x) * PIXEL_SIZE + 3] = 255;
+                if (PIXEL_SIZE > r)
+                    pattern[(y * column + x) * PIXEL_SIZE + r] = int(pow(y + column / 3.6, 1.45) * pow(x + column / 3.4, 1.55)) % 256;
+                if (PIXEL_SIZE > g)
+                    pattern[(y * column + x) * PIXEL_SIZE + g] = int(pow(y + column / 4.8, 1.15) * pow(x + column / 4.6, 1.25)) % 256;
+                if (PIXEL_SIZE > b)
+                    pattern[(y * column + x) * PIXEL_SIZE + b] = int(pow(y + column / 2.2, 1.85) * pow(x + column / 2.4, 1.75)) % 256;
+                if (PIXEL_SIZE > a)
+                    pattern[(y * column + x) * PIXEL_SIZE + a] = 255;
             }
         }
         return pattern;
     }
 
     template<int PIXEL_SIZE>
-    std::vector<unsigned char> CreateRandomPattern(const int column)
+    std::vector<unsigned char> CreateRandomPattern(const int column, const int order_rgba)
     {
-        std::vector<unsigned char> pattern(column * column * PIXEL_SIZE);
+        int r = (order_rgba & 0xFF000000) >> 24;
+        int g = (order_rgba & 0x00FF0000) >> 16;
+        int b = (order_rgba & 0x0000FF00) >>  8;
+        int a = (order_rgba & 0x000000FF) >>  0;
+        std::vector<unsigned char> pattern(column * column * 4);
         for (int y = 0; y < column; ++y)
         {
             for (int x = 0; x < column; ++x)
             {
-                int r = rand() % (256 * 256 * 256);
-                if (PIXEL_SIZE > 0)
-                    pattern[(y * column + x) * PIXEL_SIZE + 0] = ((unsigned char*)&r)[0];
-                if (PIXEL_SIZE > 1)
-                    pattern[(y * column + x) * PIXEL_SIZE + 1] = ((unsigned char*)&r)[1];
-                if (PIXEL_SIZE > 2)
-                    pattern[(y * column + x) * PIXEL_SIZE + 2] = ((unsigned char*)&r)[2];
-                if (PIXEL_SIZE > 3)
-                    pattern[(y * column + x) * PIXEL_SIZE + 3] = 255;
+                int color = rand() % (256 * 256 * 256);
+                if (PIXEL_SIZE > r)
+                    pattern[(y * column + x) * PIXEL_SIZE + r] = ((unsigned char*)&color)[0];
+                if (PIXEL_SIZE > g)
+                    pattern[(y * column + x) * PIXEL_SIZE + g] = ((unsigned char*)&color)[1];
+                if (PIXEL_SIZE > b)
+                    pattern[(y * column + x) * PIXEL_SIZE + b] = ((unsigned char*)&color)[2];
+                if (PIXEL_SIZE > a)
+                    pattern[(y * column + x) * PIXEL_SIZE + a] = 255;
             }
         }
         return pattern;
@@ -57,7 +65,6 @@ namespace steriogram
     template<int PIXEL_SIZE, int Z_LEVELS>
     void Convert(unsigned char* data, const int column, const int width, const int height, const unsigned char* pattern)
     {
-        int size = std::min(3, PIXEL_SIZE);
         std::vector<std::thread>threads(std::thread::hardware_concurrency());
         int end = 0;
         int progress = (height + threads.size() - 1) / threads.size();
@@ -80,9 +87,9 @@ namespace steriogram
                         x += side)
                     {
                         int z = 0;
-                        for (int i = 0; i < size; ++i)
+                        for (int i = 0; i < PIXEL_SIZE; ++i)
                             z += (data + (y * width + x) * PIXEL_SIZE)[i];
-                        z /= size * (256 / Z_LEVELS);
+                        z /= PIXEL_SIZE * (256 / Z_LEVELS);
                         if (z != 0)
                         {
                             for (int k = x + side * column / 2 - side * (z + z % 2 * (side + 1) / 2) / 2;
