33965be57abc8577ec398e62e910dcb49bc77efd
Ali Asadpoor <virtualmatador@yahoo.com>
2020-01-25 05:36:53 +0000

    Create run parallel function

--- stereogram.hpp
+++ stereogram.hpp
@@ -1,23 +1,41 @@
 #include <array>
-#include <vector>
 #include <cmath>
+#include <functional>
+#include <random>
 #include <thread>
+#include <vector>
 
 namespace stereogram
 {
+
+    void RunParallel(int height, std::function<void(int begin, int end)> task)
+    {
+        std::vector<std::thread>threads(std::thread::hardware_concurrency());
+        int progress = int((height + threads.size() - 1) / threads.size());
+        for (auto & thread : threads)
+        {
+            thread = std::thread([&](int index)
+            {
+                task(index * progress, std::min((index + 1) * progress, height));
+            }
+            , std::distance(threads.data(), &thread));
+        }
+        for (auto & thread : threads)
+            thread.join();
+    }
 
     int GetColumn(const int width)
     {
         return 4.4 * pow(width, 0.44);
     }
 
-    template<int PIXEL_SIZE>
-    std::vector<unsigned char> CreateCurvedPattern(const int column, const int order_rgba)
+    template<int PIXEL_SIZE, int PIXEL_FORMAT>
+    std::vector<unsigned char> CreateCurvedPattern(const int column)
     {
-        int r = (order_rgba & 0xFF000000) >> 24;
-        int g = (order_rgba & 0x00FF0000) >> 16;
-        int b = (order_rgba & 0x0000FF00) >>  8;
-        int a = (order_rgba & 0x000000FF) >>  0;
+        const int r = (PIXEL_FORMAT & 0xFF000000) >> 24;
+        const int g = (PIXEL_FORMAT & 0x00FF0000) >> 16;
+        const int b = (PIXEL_FORMAT & 0x0000FF00) >>  8;
+        const int a = (PIXEL_FORMAT & 0x000000FF) >>  0;
         std::vector<unsigned char> pattern(column * column * PIXEL_SIZE);
         for (int y = 0; y < column; ++y)
         {
@@ -36,90 +54,83 @@ namespace stereogram
         return pattern;
     }
 
-    template<int PIXEL_SIZE>
-    std::vector<unsigned char> CreateRandomPattern(const int column, const int order_rgba)
+    template<int PIXEL_SIZE, int PIXEL_FORMAT>
+    std::vector<unsigned char> CreateRandomPattern(const int column)
     {
-        int r = (order_rgba & 0xFF000000) >> 24;
-        int g = (order_rgba & 0x00FF0000) >> 16;
-        int b = (order_rgba & 0x0000FF00) >>  8;
-        int a = (order_rgba & 0x000000FF) >>  0;
+        const int r = (PIXEL_FORMAT & 0xFF000000) >> 24;
+        const int g = (PIXEL_FORMAT & 0x00FF0000) >> 16;
+        const int b = (PIXEL_FORMAT & 0x0000FF00) >>  8;
+        const int a = (PIXEL_FORMAT & 0x000000FF) >>  0;
         std::vector<unsigned char> pattern(column * column * PIXEL_SIZE);
-        for (int y = 0; y < column; ++y)
+        RunParallel(column, [&](int begin, int end)
         {
-            for (int x = 0; x < column; ++x)
+            std::random_device rd;
+            std::default_random_engine generator(rd());
+            std::uniform_int_distribution<int> distribution(0, 256 * 256 * 256 - 1);
+            for (int y = begin; y < end; ++y)
             {
-                int color = rand() % (256 * 256 * 256);
-                if (PIXEL_SIZE > r)
-                    pattern[(y * column + x) * PIXEL_SIZE + r] = ((unsigned char*)&color)[0];
-                if (PIXEL_SIZE > g)
-                    pattern[(y * column + x) * PIXEL_SIZE + g] = ((unsigned char*)&color)[1];
-                if (PIXEL_SIZE > b)
-                    pattern[(y * column + x) * PIXEL_SIZE + b] = ((unsigned char*)&color)[2];
-                if (PIXEL_SIZE > a)
-                    pattern[(y * column + x) * PIXEL_SIZE + a] = 255;
+                for (int x = 0; x < column; ++x)
+                {
+                    int color = distribution(generator) % (256 * 256 * 256);
+                    if (PIXEL_SIZE > r)
+                        pattern[(y * column + x) * PIXEL_SIZE + r] = ((unsigned char*)&color)[0];
+                    if (PIXEL_SIZE > g)
+                        pattern[(y * column + x) * PIXEL_SIZE + g] = ((unsigned char*)&color)[1];
+                    if (PIXEL_SIZE > b)
+                        pattern[(y * column + x) * PIXEL_SIZE + b] = ((unsigned char*)&color)[2];
+                    if (PIXEL_SIZE > a)
+                        pattern[(y * column + x) * PIXEL_SIZE + a] = 255;
+                }
             }
         }
+        );
         return pattern;
     }
 
     template<int PIXEL_SIZE, int Z_LEVELS>
     void Convert(unsigned char* data, const int column, const int width, const int height, const unsigned char* pattern)
     {
-        std::vector<std::thread>threads(std::thread::hardware_concurrency());
-        int end = 0;
-        int progress = int((height + threads.size() - 1) / threads.size());
-        for (auto & thread : threads)
+        RunParallel(height, [&](int begin, int end)
         {
-            int start = end;
-            end += progress;
-            bool run = end < height;
-            if (!run)
-                end = height;
-            thread = std::thread([&, start, end]()
+            std::vector<int> shifts(width);
+            for (int y = begin; y < end; ++y)
             {
-                std::vector<int> shifts(width);
-                for (int y = start; y < end; ++y)
+                std::fill(shifts.begin(), shifts.end(), 0);
+                int side = (y % 2) * 2 - 1;
+                for (int x = width / 2 - side * width / 2 - (1 - side) / 2 + side * column;
+                    side * x <= side * (width / 2 + side * width / 2 - (1 + side) / 2 - side * column);
+                    x += side)
                 {
-                    std::fill(shifts.begin(), shifts.end(), 0);
-                    int side = (y % 2) * 2 - 1;
-                    for (int x = width / 2 - side * width / 2 - (1 - side) / 2 + side * column;
-                        side * x <= side * (width / 2 + side * width / 2 - (1 + side) / 2 - side * column);
-                        x += side)
+                    int z = 0;
+                    for (int i = 0; i < 3; ++i)
+                        z += (data + (y * width + x) * PIXEL_SIZE)[i];
+                    z /= 3 * (256 / Z_LEVELS);
+                    if (z != 0)
                     {
-                        int z = 0;
-                        for (int i = 0; i < 3; ++i)
-                            z += (data + (y * width + x) * PIXEL_SIZE)[i];
-                        z /= 3 * (256 / Z_LEVELS);
-                        if (z != 0)
+                        for (int k = x + side * column / 2 - side * (z + z % 2 * (side + 1) / 2) / 2;
+                            side * k <= side * (width / 2 + side * width / 2 - (1 + side) / 2); k += side * column)
                         {
-                            for (int k = x + side * column / 2 - side * (z + z % 2 * (side + 1) / 2) / 2;
-                                side * k <= side * (width / 2 + side * width / 2 - (1 + side) / 2); k += side * column)
-                            {
-                                shifts[k] = side * z;
-                                int target = k + side * z;
-                                if (target < 0)
-                                    target += column;
-                                else if (target >= width)
-                                    target -= column;
-                                shifts[k] += shifts[target];
-                                if (shifts[k] < 0)
-                                    shifts[k] += column;
-                                else if (shifts[k] >= column)
-                                    shifts[k] -= column;
-                            }
+                            shifts[k] = side * z;
+                            int target = k + side * z;
+                            if (target < 0)
+                                target += column;
+                            else if (target >= width)
+                                target -= column;
+                            shifts[k] += shifts[target];
+                            if (shifts[k] < 0)
+                                shifts[k] += column;
+                            else if (shifts[k] >= column)
+                                shifts[k] -= column;
                         }
                     }
-                    for (int x = 0; x < width; ++x)
-                        memcpy(data + (y * width + x) * PIXEL_SIZE,
-                            pattern + ((y % column) * column + ((x + shifts[x]) % column)) * PIXEL_SIZE,
-                            PIXEL_SIZE);
                 }
-            });
-            if (!run)
-                break;
+                for (int x = 0; x < width; ++x)
+                    memcpy(data + (y * width + x) * PIXEL_SIZE,
+                        pattern + ((y % column) * column + ((x + shifts[x]) % column)) * PIXEL_SIZE,
+                        PIXEL_SIZE);
+            }
         }
-        for (auto & thread : threads)
-            thread.join();
+        );
     }
 
 }
